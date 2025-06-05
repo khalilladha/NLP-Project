23bb7ef8273d7f084ca0e630d72b38fd0cf1254a
Khalil Ladha <khalilullah001@gmail.com>
2025-06-05 16:51:03 +0530

    Third commit

--- demo.py
+++ demo.py
@@ -3,18 +3,21 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 import os
 load_dotenv()
-
+from langchain_community.llms import Ollama
 
 #os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 ## Langsmith Tracking
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-llm_model=ChatOpenAI(model='gpt-4o-mini',temperature=0.7)
-
+llm_model=ChatOpenAI(model='gpt-4o-mini')
 st.title("Open AI Model")
+key=st.text_input("Please enter your OpenAI Key")
 input=st.text_input("Enter your prompt")
-button=st.button("Click")
-if button:
+if key and input:
+    os.environ['OPENAI_API_KEY']=key
     result=llm_model.invoke(input)
     st.text_area("Response", str(result))
+
+   
+   
 
